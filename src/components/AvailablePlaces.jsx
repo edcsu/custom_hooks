ce844590518cc8caa2c92903dd4691e1b1574cbd
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-14 20:03:18 +0300

    refactor: use custom hook in the app throughout

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,17 +1,30 @@
-import { useState, useEffect } from 'react';
-
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
 import {useFetch} from '../hooks/useFetch.js'
 
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces()
+
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      
+      resolve(sortedPlaces);
+    });
+  })
+}
+
 export default function AvailablePlaces({ onSelectPlace }) {
 
   const { isFetching, 
     error, 
-    fetchedData: availablePlaces, 
-    setFetchedData: setAvailablePlaces} = useFetch(fetchAvailablePlaces, [])
+    fetchedData: availablePlaces} = useFetch(fetchSortedPlaces, [])
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
